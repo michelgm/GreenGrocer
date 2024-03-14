4189ca9c2df839968c5a0309a6c588655cb06644
michelgm <99933941+michelgm@users.noreply.github.com>
2024-03-14 11:18:00 -0300

    Base Screen

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:greengrocer/src/auth/config/custom_colors.dart';
+import 'package:greengrocer/src/base/base_screen.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
 import 'package:greengrocer/src/auth/sign_up_screen.dart';
 
 import 'components/custom_text_field.dart';
@@ -110,7 +111,13 @@ class SignInScreen extends StatelessWidget {
                             borderRadius: BorderRadius.circular(18),
                           ),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (c) {
+                              return const BaseScreen();
+                            }),
+                          );
+                        },
                         child: const Text(
                           'Entrar',
                           style: TextStyle(
