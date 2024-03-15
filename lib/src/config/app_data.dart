3e15fcf0c34950d2851428b83a78f661b78fb090
michelgm <99933941+michelgm@users.noreply.github.com>
2024-03-15 14:11:10 -0300

    refatoring paths

---
+++ lib/src/config/app_data.dart
@@ -0,0 +1,10 @@
+import 'package:greengrocer/src/models/item_model.dart';
+
+ItemModel apple = ItemModel(
+  description:
+      "A melhor maçã da região e que conta com o melhor preço de qualquer quitanda",
+  imgUrl: imgUrl,
+  itemName: 'Maçã',
+  price: 5.5,
+  unit: 'kg',
+);
